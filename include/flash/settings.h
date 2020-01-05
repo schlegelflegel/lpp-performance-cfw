b8876d71c8c466ab448d29d7ddd5ebf487de0a11
mat1jaczyyy <mat1jaczyyy@gmail.com>
2020-01-05 08:05:20 +0100

    Small bootloader dump app

--- include/flash/settings.h
+++
@@ -1,9 +0,0 @@
-#ifndef SETTINGS_H
-#define SETTINGS_H
-
-#include "app_defs.h"
-#include "led/palettes.h"
-
-u8 palette_selected, vel_sensitive, top_lights_config, performance_xy_enabled, aftertouch_enabled, direct_enabled, idle_enabled, led_brightness;
-
-#endif
--- src/app.c
+++ src/app.c
@@ -1,126 +1,50 @@
-/*----------------------------------------------------------------------------*/
-/*  Performance-optimized Launchpad Pro Firmware modification by mat1jaczyyy  */
-/*----------------------------------------------------------------------------*/
-
-/******************************************************************************
- 
-  Copyright (c) 2015, Focusrite Audio Engineering Ltd.
-  All rights reserved.
- 
-  Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions are met:
- 
- * Redistributions of source code must retain the above copyright notice, this
- list of conditions and the following disclaimer.
- 
- * Redistributions in binary form must reproduce the above copyright notice,
- this list of conditions and the following disclaimer in the documentation
- and/or other materials provided with the distribution.
- 
- * Neither the name of Focusrite Audio Engineering Ltd., nor the names of its
- contributors may be used to endorse or promote products derived from
- this software without specific prior written permission.
- 
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- 
- ******************************************************************************/
 
 #include "app.h"
 
-u32 global_timer = 0;
-u32 idle_time = idle_timeout;
+#define MAX 0x20000
+#define SINGLE 16
+#define SPEED 8
 
-void idle_exit() {
-	if (mode == mode_idle) mode_update(idle_return);
-	idle_time = global_timer + idle_timeout;
-}
+u32 global_timer = 0;
+u8 started = 0;
+
+u8 send_buf[SINGLE * 2 + 2] = {0xF0};
 
 void app_timer_event() {
-	global_timer++;
+	if (!started) return;
+	
+	if (global_timer++ % (SINGLE * SPEED) == 0) {
+		for (u32 i = 0; i < SINGLE; i++) {
+			u8 val = *(u8*)(0x08000000 + global_timer / SPEED + i);
+			send_buf[i * 2 + 1] = val >> 4;
+			send_buf[i * 2 + 2] = val & 0x0F;
+		}
+		
+		hal_send_sysex(USBMIDI, &send_buf[0], SINGLE * 2 + 2);
 
-	if (idle_enabled && mode < mode_normal - 1 && idle_time <= global_timer) {
-		idle_return = mode;
-		mode_update(mode_idle);
+		for (u8 i = 0; i <= global_timer / SPEED * SINGLE / MAX; i++) {
+			hal_plot_led(TYPEPAD, (i / 8 + 1) * 10 + (i % 8 + 1), 63, 63, 63);
+		}
+
+		if (global_timer / SPEED >= MAX) {
+			global_timer = 0;
+			started = 0;
+		}
 	}
-
-	tempo_timer++; tempo_tick();
-
-	(*mode_timer_event[mode])();
 }
 
 void app_surface_event(u8 t, u8 p, u8 v) {
-	idle_exit();
-
-	if (!vel_sensitive) {
-		v = (v == 0)? 0 : 127;
-	}
-
-	(*mode_surface_event[mode])(p, v, p / 10, p % 10);
+	started = 1;
 }
 
-void app_midi_event(u8 port, u8 t, u8 p, u8 v) {
-	idle_exit();
+void app_midi_event(u8 port, u8 t, u8 p, u8 v) {}
 
-	u8 ch;
+void app_aftertouch_event(u8 p, u8 v) {}
 
-	switch (t) {
-		case 0xFA:
-			tempo_start();
-			break;
+void app_cable_event(u8 t, u8 v) {}
 
-		case 0xF8:
-			tempo_midi();
-			break;
-		
-		case 0xFC:
-			tempo_stop();
-			break;
-		
-		default:
-			ch = t % 16;
-			t >>= 4;
-
-			if (mode != mode_ableton && mode_default == mode_ableton) {
-				(*mode_midi_event[mode_default])(port, t, ch, p, v);
-			}
-
-			(*mode_midi_event[mode])(port, t, ch, p, v);
-			break;
-	}
-}
-
-void app_aftertouch_event(u8 p, u8 v) {
-	idle_exit();
-
-	u8 result = aftertouch_update(p, v);
-
-	if (result != -1 && aftertouch_enabled == 1)
-		(*mode_aftertouch_event[mode])(result);
-	
-	if (aftertouch_enabled == 2)
-		(*mode_poly_event[mode])(p, v);
-}
-
-void app_cable_event(u8 t, u8 v) {
-	idle_exit();
-}
-
-void app_sysex_event(u8 port, u8 *d, u16 l) {
-	idle_exit();
-
-	handle_sysex(port, d, l);
-}
+void app_sysex_event(u8 port, u8 *d, u16 l) {}
 
 void app_init(const u16 *adc_raw) {
-	flash_read();
-	mode_update(mode_boot);
+	send_buf[SINGLE * 2 + 1] = 0xF7;
 }
